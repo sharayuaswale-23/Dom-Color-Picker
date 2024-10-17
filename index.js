1e78d683912cabe4800c47211e07f34ce9b574ca
sharayuaswale-23 <sharayuaswale123@gmail.com>
2024-10-17 17:32:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let colorNameEl = document.getElementById("colorName");
+let mainPageEl = document.getElementById("mainPage");
+
+function colorchange(){
+    mainPageEl.style.backgroundColor = "#e0e0e0";
+    colorNameEl.textContent = "#e0e0e0";
+}
+function colorchange2(){
+    mainPageEl.style.backgroundColor = "#6fcf97";
+    colorNameEl.textContent = "#6fcf97";
+}
+function colorchange3(){
+    mainPageEl.style.backgroundColor = "#56ccf2";
+    colorNameEl.textContent = "#56ccf2";
+}
+function colorchange4(){
+    mainPageEl.style.backgroundColor = "#bb6bd9";
+    colorNameEl.textContent = "#bb6bd9";
+}
